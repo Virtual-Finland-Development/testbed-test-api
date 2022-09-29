b95a40d5632d099c5176e2a35bf92263852aedd8
lassi.patanen <lassi.patanen@gofore.com>
2022-09-29 14:53:51 +0300

    Added TMT productizer to testbed API

--- testbed.js
+++ testbed.js
@@ -1,5 +1,5 @@
 "use strict";
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 module.exports.getPopulation = async (event) => {
   const body = JSON.parse(event.body);
@@ -14,7 +14,7 @@ module.exports.getPopulation = async (event) => {
   }
 
   const { city, year } = body;
-  
+
   const testbedResponse = await fetch(
     "https://gateway.testbed.fi/test/lsipii/Figure/Population?source=virtual_finland",
     {
@@ -32,5 +32,38 @@ module.exports.getPopulation = async (event) => {
   return {
     statusCode: testbedData?.status || 200,
     body: JSON.stringify(testbedData),
+  };
+};
+
+module.exports.findJobPostings = async (event) => {
+  const body = JSON.parse(event.body);
+
+  if (!body) {
+    return {
+      statusCode: 422,
+      body: JSON.stringify({
+        message: "Request body is missing",
+      }),
+    };
+  }
+
+  const response = await fetch(
+    "https://gateway.testbed.fi/test/lassipatanen/Job/JobPosting?source=tyomarkkinatori",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          event.headers.authorization || event.headers.Authorization,
+      },
+      body: event.body,
+    }
+  );
+
+  let responseJson = await response.json();
+
+  return {
+    statusCode: responseJson?.status || 200,
+    body: JSON.stringify(responseJson),
   };
 };
